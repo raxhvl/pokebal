4d1ef50836b1e718c382a624cd42b0653d72c943
raxhvl <raxhvl@users.noreply.github.com>
2025-10-17 03:46:30 +0000

    ✨ feat: Bal v1.3.0

--- web/src/config/app.ts
+++ web/src/config/app.ts
@@ -18,7 +18,7 @@ export const config = {
 		// See: https://eest.ethereum.org/main/running_tests/hive/common_options/
 		parallelism: 4,
 		buildArgs: {
-			fixtures: "bal@v1.2.0",
+			fixtures: "bal@v1.3.0",
 			branch: "main",
 		},
 		testFilter: "id:tests/amsterdam/eip7928_block_level_access_lists",
